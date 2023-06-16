57ec7aa880185980472161e96a29ac652ddbf9ae
patrick smith <smithpatrick.510@gmail.com>
2023-06-16 17:12:32 -0400

    variables exercise

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Declare and Initialize Variables
+string name = "John";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+                  
+
+
+
+
